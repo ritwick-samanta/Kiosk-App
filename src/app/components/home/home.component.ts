ad08adcd1db8230fd3f6f181bd23b4290844ba07
Ritwick Samanta <ritwick.samanta@ba.com>
2024-03-13 10:23:14 +0000

    Added Home Page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { MatButtonModule } from '@angular/material/button';
+import { LoginService } from '../../services/login.service';
+
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [NavbarComponent,MatButtonModule]
 })
 export class HomeComponent {
-
+ constructor(private _login:LoginService){}
+ login(){
+    this._login.doLogin();
+ }
 }
--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,16 +1,48 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { NgIf,CommonModule, IMAGE_CONFIG } from '@angular/common';
 @Component({
   selector: 'app-splash',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './splash.component.html',
-  styleUrl: './splash.component.css'
+  styleUrl: './splash.component.css',
+  animations:[
+    trigger('fadeIn', [
+      
+      transition('void=>*', [
+        style({opacity:0}),
+        animate('1s ease-out',style({opacity:1}))
+      ])
+      
+  ]
+  ),
+trigger('fadeOut',[
+  transition('void=>*', [
+    style({opacity:1}),
+    animate('1s ease-in-out',style({opacity:0}))
+  ])
+])],
+providers: [
+  {
+    provide: IMAGE_CONFIG,
+    useValue: {
+      disableImageSizeWarning: true
+    }
+  },
+]
 })
 export class SplashComponent implements OnInit {
+  image:String;
   constructor(private router:Router){}
-  ngOnInit(): void {
-      setTimeout(()=>this.router.navigate(['/home']),10000);
+  visibility=true;
+  async ngOnInit(): Promise<void> {
+    this.image='assets/images/BA_LOGO.png';
+    setTimeout(() => {
+      this.visibility = false;
+    }, 1000)
+    setTimeout(() => this.router.navigate(['/home']), 2000)
   }
+  
 }
